a200cd5fb7a35772fcadf249092c18bf6654eb60
marcVS <marc@paralox.de>
2023-11-16 22:30:10 +0100

    test marc

--- logic/strategy.py
+++ logic/strategy.py
@@ -2,7 +2,11 @@ from models.bet import Bet
 from models.table import Table
 
 
-def decide(table: Table) -> Bet:
-    # TODO: Add Poker Logic Here... :)
+#def decide(table: Table) -> Bet:
 
-    return Bet(20)
+ #   return Bet(20)
+
+
+def decide(table: Table) -> Bet:
+    # Deine Implementierung hier
+    return Bet(bet=table.players[table.activePlayer].stack)

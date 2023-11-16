dfd32fdfefb644f1cdc03e1ff7d7250b1daa8a44
marcVS <marc@paralox.de>
2023-11-16 22:44:20 +0100

    .

--- logic/strategy.py
+++ logic/strategy.py
@@ -3,4 +3,5 @@ from models.table import Table
 
 def decide(table: Table) -> Bet:
     player = table.players[table.activePlayer]
-    bet_amount = int(0.7 * player.stack) 
+
+    bet_amount = int(player.stack) 

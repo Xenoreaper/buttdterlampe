90d7fbc0c7000aab0fc4539c88b5266273583c0e
marcVS <marc@paralox.de>
2023-11-16 22:40:38 +0100

    0.7

--- logic/strategy.py
+++ logic/strategy.py
@@ -1,12 +1,6 @@
 from models.bet import Bet
 from models.table import Table
 
-
-#def decide(table: Table) -> Bet:
-
- #   return Bet(20)
-
-
 def decide(table: Table) -> Bet:
     player = table.players[table.activePlayer]
-    bet_amount = int(0.2 * player.stack)  # Setzt 20% des Chip-Stacks
+    bet_amount = int(0.8 * player.stack) 
